ae238d07afd624416145908a11e21cb29b5a4b82
AuntL1ke <88139445+AuntL1ke@users.noreply.github.com>
2024-04-21 06:38:41 -0700

    Add files via upload

---
+++ Logic/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Mystery,
+        Romance,
+        ScienceFiction,
+        Fantasy,
+        Thriller,
+        Horror,
+        Biography,
+        History,
+        Poetry,
+        Drama,
+        Other
+    }
+
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public int? Pages { get; set; }
+        public int? Year { get; set; }
+        public int? Cost { get; set; }
+        public int? Price { get; set; }
+
+        public bool? IsPart { get; set; }
+        public Genre? Genre { get; set; }
+        public int? IsDiscount { get; set; }
+        public bool? IsNew { get; set; }
+    }
+
+    public class Purchase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public int BookId { get; set; }
+
+        public int Total {  get; set; }
+        public Book Book { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+
+
+}
---
+++ Logic/BookShop.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class MegaBookShop : DbContext
+    {
+        public MegaBookShop() { Database.EnsureCreated(); }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-JGNME93;
+                                    initial catalog=BookShop;
+                                    integrated security=True;
+                                    Connect Timeout = 2;
+                                    Encrypt = False;
+                                    Trust Server Certificate = False;
+                                    Application Intent = ReadWrite;
+                                    Multi Subnet Failover = False");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
